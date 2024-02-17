261dda40f670375838ca0c525b3d49a33cd17066
Rishabh Kumar Jha <rishavmalhotra761@gmail.com>
2024-02-17 10:21:40 +0530

    getElementsByClassName Method

---
+++ DOM Manipulations/getElementsByClassName Method/index.js
@@ -0,0 +1,15 @@
+const fruit = document.getElementsByClassName('fruit');
+console.log(fruit[2]);
+fruit[2].style.backgroundColor='yellow';
+
+console.log(fruit[0]);
+fruit[0].style.fontWeight='bold';
+
+console.log(fruit[1]);
+fruit[1].style.fontWeight='bold';
+
+console.log(fruit[2]);
+fruit[2].style.fontWeight='bold';
+
+console.log(fruit[3]);
+fruit[3].style.fontWeight='bold';
