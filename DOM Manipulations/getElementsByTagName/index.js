7788443e326c8aeaa155b8e1f3505543a12af75b
Rishabh Kumar Jha <rishavmalhotra761@gmail.com>
2024-02-17 10:25:44 +0530

    getElementsByTagName

---
+++ DOM Manipulations/getElementsByTagName/index.js
@@ -0,0 +1,19 @@
+const fruit = document.getElementsByClassName('fruit');
+console.log(fruit[2]);
+fruit[2].style.backgroundColor='yellow';
+
+console.log(fruit[0]);
+fruit[0].style.fontStyle='italic';
+
+console.log(fruit[1]);
+fruit[1].style.fontStyle='italic';
+
+console.log(fruit[2]);
+fruit[2].style.fontStyle='italic';
+
+console.log(fruit[3]);
+fruit[3].style.fontStyle='italic';
+
+console.log(fruit[4]);
+fruit[4].style.color='blue';
+fruit[4].style.fontStyle='italic';
